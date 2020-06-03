f9f712833fe300b2cc18810c8ea61874c56ca05e
JohnNils Olson <johnnils@gmail.com>
2020-06-03 14:06:13 -0700

    add script for sub, mult, div
    Co-authored-by: Teresa Rosinski <trosinski89@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -20,8 +20,30 @@ $(document).ready(function() {
 		const number1 = parseInt($("#add1").val());
 		const number2 = parseInt($("#add2").val());
 		const result = add(number1, number2);
-		$("#output").text(result);
+		$("#output-add").text(result);
+	});
+
+	$("form#sub").submit(function(event) {
+		event.preventDefault();
+		const number1 = parseInt($("#sub1").val());
+		const number2 = parseInt($("#sub2").val());
+		const result = sub(number1, number2);
+		$("#output-sub").text(result);	
+	});
+
+	$("form#mult").submit(function(event) {
+		event.preventDefault();
+		const number1 = parseInt($("#mult1").val());
+		const number2 = parseInt($("#mult2").val());
+		const result = mult(number1, number2);
+		$("#output-mult").text(result);	
+	});
+
+	$("form#div").submit(function(event) {
+		event.preventDefault();
+		const number1 = parseInt($("#div1").val());
+		const number2 = parseInt($("#div2").val());
+		const result = div(number1, number2);
+		$("#output-div").text(result);	
 	});
 });
-
-
