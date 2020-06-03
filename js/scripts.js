1249df934f2a04d0bfc1950e369aa374a996cee2
JohnNils Olson <johnnils@gmail.com>
2020-06-03 13:08:29 -0700

    add link to scripts, styles, bootstrap, jQuery
    Co-authored-by: Teresa Rosinski <trosinski89@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,19 @@ const add = function(number1, number2) {
 	return number1 + number2;
 };
 
+const sub = function(number1, number2) {
+	return number1 - number2;
+};
+
+const mult = function(number1, number2) {
+	return number1 * number2;
+};
+
+const div = function(number1, number2) {
+	return number1 / number2;
+};
+
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter a number:"));
 
-alert(add(number1, number2));
+alert((number1, number2));
